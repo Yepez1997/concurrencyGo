882f3fb2c2ed8f48eb614a6dc7fef618f15a0ec7
Yepez1997 <yepez4845@gmail.com>
2019-06-06 11:18:46 -0700

    cond

---
+++ src/condQueue/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+
+
+func main() {
+	c := sync.NewCond(&sync.Mutex{}}
+	queue := make([]interface{}, 0, 10)
+
+	removeFromQueue := func(delay time.Duration) {
+		time.Sleep(delay)
+		c.L.Lock()
+		queue = queue[1:]
+		fmt.Println("Removed from the queue")
+		c.L.Unlock()
+		c.Signal()
+	}
+
+	for i := 0; i < 10; i++ {
+		c.L.Lock()
+		for len(queue) == 2 {
+			c.Wait()
+		}
+		fmt.Println("Addong to the queue")
+		queue = append(queue, struct{}{})
+		go removeFromQueue(1 * time.Second)
+		c.L.Lock()
+	}
+
+}
