9ecca089d0fd93e93b438d1fddc63aac416e2c98
Yepez1997 <yepez4845@gmail.com>
2019-06-15 12:42:18 -0700

    bridge channel

--- src/orDoneChannel/main.go
+++ src/orDoneChannel/main.go
@@ -22,5 +22,4 @@ func main() {
 		}()
 		return valStream
 	}
-
 }
---
+++ src/teeChannel/main.go
@@ -0,0 +1,25 @@
+package main 
+
+func main() {
+	tee := func(done <-chan interface{}, in <-chan interface) (_,_,<-chan interface{}) {
+		out1 := make(chan interface{})
+		out2 := make(chan interface{})
+		go func() {
+			defer close(out1)
+			defer close(out2)
+			for val := range orDone(done, in) {
+				var out1, out2 = out1, out2
+				for i := 0; i < 2; i++ {
+					select {
+					case <-done:
+					case out1<-val:
+						out1 = nil 
+					case out2<-val:
+						out2 = nil 
+					}
+				}
+			}
+		}()
+	}
+	return out1, out2
+}
