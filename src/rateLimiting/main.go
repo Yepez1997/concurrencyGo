48645d36d297de194131c731c7ee2a2d314e5086
Yepez1997 <yepez4845@gmail.com>
2019-08-31 16:15:56 -0700

    updates

--- src/rateLimiting/main.go
+++ src/rateLimiting/main.go
@@ -4,19 +4,58 @@ import (
 	"context"
 	"log"
 	"os"
+	"sort"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func Per(eventCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration / time.Duration(eventCount))
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+// since multiLimiter contains the interface RateLimiter then we have to implement the interface
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
 func Open() *APIConnection {
+	secondLimit := rate.NewLimiter(Per(2, time.Second), 1)
+	minuteLimit := rate.NewLimiter(Per(10, time.Minute), 10)
 	return &APIConnection{
-		rateLimiter: rate.NewLimiter(rate.Limit(1), 1),
+		rateLimiter: MultiLimiter(secondLimit, minuteLimit),
 	}
 }
 
 type APIConnection struct {
-	rateLimiter *rate.Limiter
+	rateLimiter RateLimiter
 }
 
 func (a *APIConnection) ReadFile(ctx context.Context) error {
