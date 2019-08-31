68b674e86cb8e91884c1f5a174216ef890a637bd
Yepez1997 <yepez4845@gmail.com>
2019-08-30 20:23:37 -0700

    mult limiter example

---
+++ src/multiLimiter/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"sort"
+
+	"golang.org/x/time/rate"
+)
+
+// RateLimiter Interface
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+// since multiLimiter contains the interface RateLimiter then we have to implement the interface
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
+// multiLimiter example
+func main() {
+
+}
