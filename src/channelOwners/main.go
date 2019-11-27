9ba184636e1c9e016a034cc7a8453f5581bcc967
Aureliano Yepez <yepez4845@gmail.com>
2019-11-26 22:41:12 -0800

    concurreny

--- src/channelOwners/main.go
+++ src/channelOwners/main.go
@@ -9,15 +9,17 @@ func main() {
 		go func() {
 			defer close(resultStream)
 			for i := 0; i <= 5; i++ {
+				// send the item to the channel
 				resultStream <- i
 			}
 		}()
 		return resultStream
 	}
-
+	// result stream is a stream of ints
 	resultStream := chanOwner()
+	// for loop reads from the channel
 	for result := range resultStream {
-		fmt.Printf("Received %d\ n", result)
+		fmt.Printf("Received %d\n", result)
 	}
 	fmt.Println("Done receiving")
 
